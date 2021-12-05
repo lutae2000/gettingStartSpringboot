580cfd5a083badfe7f32b9cdb3d96b9be790bc96
lutae2000 <lutae2000@gmail.com>
2021-12-06 01:39:18 +0900

    스프링부트 스터디

---
+++ src/main/java/com/example/springStudy/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.example.springStudy.repository;
+
+import com.example.springStudy.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+    void clearStore();
+}
